b5eebbbc316a74be35120b1acd36d0b10bdc500f
Mogra12 <86124740+Mogra12@users.noreply.github.com>
2023-11-17 14:56:56 -0300

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,16 @@
+from anyplayer import get_player
+import time
+import os
+
+
+def counter():
+    os.system("cls")
+    temp = int(input("type the time: "))
+    temp_machine = 0
+    while temp_machine <= temp: 
+        print(temp_machine)
+        temp_machine += 1
+        time.sleep(1)
+        os.system("cls")
+    print("countdown was finished")
+counter()
